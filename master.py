2af662cb2611544d0aaed4e3ce5e8afeded36c82
borya <b.kisselev@gmail.com>
2024-01-01 20:27:41 +0200

    fullsceen image

--- master.py
+++ master.py
@@ -1,1 +1,47 @@
-print('тут будет the best game')
+import os
+import sys
+import pygame
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def main():
+    pygame.init()
+
+    screen_info = pygame.display.Info()
+    screen_w = screen_info.current_w
+    screen_h = screen_info.current_h
+
+    all_sprites = pygame.sprite.Group()
+
+    image = load_image("backround.jpg")
+    hero_image = pygame.transform.scale(image, (screen_w, screen_h))
+    hero = pygame.sprite.Sprite(all_sprites)
+    hero.image = hero_image
+    hero.rect = hero.image.get_rect()
+    hero.rect.x, hero.rect.y = 0, 0
+
+    screen = pygame.display.set_mode((screen_w, screen_h), pygame.FULLSCREEN)
+    pygame.display.set_caption('Game')
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        all_sprites.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
