31e318a97d2db4663271e72c44b846a3680362b3
borya <b.kisselev@gmail.com>
2024-01-01 21:19:03 +0200

    move

--- master.py
+++ master.py
@@ -12,12 +12,31 @@ def load_image(name, colorkey=None):
     return image
 
 
+class sqr:
+    def __init__(self):
+        self.cords = ()
+        self.x = 410
+        self.y = 540
+
+    def __call__(self, screen, *args, **kwargs):
+        self.x += args[0]
+        self.y += args[1]
+        pygame.draw.rect(screen, 'red',(self.x, self.y, 100, 150), 8)
+        self.cords = (self.x, self.y)
+
+    def get_cords(self):
+        return self.cords
+
+
 def main():
     pygame.init()
 
     screen_info = pygame.display.Info()
     screen_w = screen_info.current_w
     screen_h = screen_info.current_h
+
+    screen = pygame.display.set_mode((screen_w, screen_h), pygame.FULLSCREEN)
+    pygame.display.set_caption('Game')
 
     all_sprites = pygame.sprite.Group()
 
@@ -28,18 +47,35 @@ def main():
     hero.rect = hero.image.get_rect()
     hero.rect.x, hero.rect.y = 0, 0
 
-    screen = pygame.display.set_mode((screen_w, screen_h), pygame.FULLSCREEN)
-    pygame.display.set_caption('Game')
+    all_sprites.draw(screen)
 
+    clock = pygame.time.Clock()
+    obj = sqr()
+    obj(screen, 0, 0)
+    cords = (460, 690)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                cords = event.pos
+        if cords[0] != obj.get_cords()[0] + 50 or cords[1] != obj.get_cords()[1] + 150:
+            x = 0
+            y = 0
+            if cords[0] > obj.get_cords()[0] + 50:
+                x = 1
+            if cords[0] < obj.get_cords()[0] + 50:
+                x = -1
+            if cords[1] > obj.get_cords()[1] + 150:
+                y = 1
+            if cords[1] < obj.get_cords()[1] + 150:
+                y = -1
+            all_sprites.draw(screen)
+            obj(screen, x, y)
 
-        all_sprites.draw(screen)
+        clock.tick(150)
         pygame.display.flip()
-
     pygame.quit()
 
 
