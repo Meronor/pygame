ad0be8a3e39c7195d2ac7dd92f598fe862a0a391
Borya <B.kisselev@gmail.com>
2024-01-10 19:17:00 +0200

    Разбивка программы на логические блоки.Добавил функцию game и комментарии. Спасибо Вадик:)

--- core/handlers/base.py
+++ core/handlers/base.py
@@ -177,3 +177,23 @@ def game_update(pygame, screen, all_sprites, hero, cords, clock):
 
     # Отображение новых изменений (перерисовка)
     pygame.display.flip()
+
+
+def game(pygame):
+    # получаем размер экрана
+    # Конфигурация экрана
+    screen, pixels, all_sprites, screen_w, screen_h = screen_init(pygame)
+
+    # Получение всех героев в кортеже
+    hero, bg, bg_image, objects = objects_init(pygame, screen_w, screen_h)
+
+    # Задание значений игровых переменных
+    running, barrier, isImpasse, clock, cords, dx, dy = \
+        game_init(screen, hero, bg, all_sprites, objects, screen_w, screen_h)
+
+    while running:
+        running, cords, bg_image, pixels = event_handling(pygame.event.get(), hero, bg, bg_image, pixels, cords,
+                                                          screen_w, screen_h)
+        barrier, isImpasse, dx, dy = step_handling(pixels, cords, hero, barrier, isImpasse, dx, dy)
+
+        game_update(pygame, screen, all_sprites, hero, cords, clock)
--- master.py
+++ master.py
@@ -1,29 +1,14 @@
 import pygame
 
-from core.handlers.base import corners, screen_init, objects_init, game_init, event_handling, game_update, step_handling
+from core.handlers.base import game
 
 
 def main():
+    # Конфигурация игрового движка
     pygame.init()
-
-    # получаем размер экрана
-    # Конфигурация экрана
-    screen, pixels, all_sprites, screen_w, screen_h = screen_init(pygame)
-
-    # Получение всех героев в кортеже
-    hero, bg, bg_image, objects = objects_init(pygame, screen_w, screen_h)
-
-    # Задание значений игровых переменных
-    running, barrier, isImpasse, clock, cords, dx, dy = \
-        game_init(screen, hero, bg, all_sprites, objects, screen_w, screen_h)
-
-    while running:
-        running, cords, bg_image, pixels = event_handling(pygame.event.get(), hero, bg, bg_image, pixels, cords,
-                                                          screen_w, screen_h)
-        barrier, isImpasse, dx, dy = step_handling(pixels, cords, hero, barrier, isImpasse, dx, dy)
-
-        game_update(pygame, screen, all_sprites, hero, cords, clock)
-
+    # Игровой цикл
+    game(pygame)
+    # Выход из приложения
     pygame.quit()
 
 
