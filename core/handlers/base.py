f3b4ea5f6906b089113f68276afe4c31132012a3
Borya <B.kisselev@gmail.com>
2024-01-10 18:55:57 +0200

    разбиваю весь код на функции, переход к функциональному программированию

--- core/handlers/base.py
+++ core/handlers/base.py
@@ -2,6 +2,11 @@ import os
 import sys
 import pygame
 
+# Импорт объектов-героев
+from core.handlers.items import Hero, Entity, Object
+# Получение констант из конфигурации
+from core.data.constant import dS
+
 
 def load_image(name):
     fullname = os.path.join('core/data', name)
@@ -28,3 +33,108 @@ def corners(pos1, pos2):
         return 1, 1
     else:
         return 0, 0
+
+
+def event_handling(events, hero, bg, bg_image, pixels, cords, screen_w, screen_h):
+    for event in events:
+        # выход из программы при нажатии на крестик
+        if event.type == pygame.QUIT:
+            return False, cords, bg_image, pixels
+
+        # выход из программы по клавише Esc
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                return False, cords, bg_image, pixels
+
+        # проверка получения новых координат для героя
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # новые требуемые координаты героя
+            bg_image, pixels = background(hero, bg, bg_image, pixels, event.pos, screen_w, screen_h)
+            hero.need_rotate(event.pos)
+            return True, event.pos, bg_image, pixels
+    return True, cords, bg_image, pixels
+
+
+def background(hero, bg, bg_image, pixels, cords, screen_w, screen_h):
+    if (pixels[cords] == 254 and hero.get_cords()[0] < screen_w * 0.2 and
+            bg_image != "background_river.jpg"):
+        bg_image = "background_river.jpg"
+        bg.image = pygame.transform.scale(load_image(bg_image), (screen_w, screen_h))
+        wb_bg_image = pygame.transform.scale(load_image("wb_background_river.jpg"), (screen_w, screen_h))
+        pixels = pygame.PixelArray(wb_bg_image)
+        hero.change_rect(screen_w * 0.85, screen_h * 0.75)
+        return bg_image, pixels
+    if (pixels[cords] == 254 and hero.get_cords()[0] > screen_w * 0.7 and
+            bg_image != "backround.jpg"):
+        bg_image = "backround.jpg"
+        bg.image = pygame.transform.scale(load_image(bg_image), (screen_w, screen_h))
+        wb_bg_image = pygame.transform.scale(load_image("wb_backround.jpg"), (screen_w, screen_h))
+        pixels = pygame.PixelArray(wb_bg_image)
+        hero.change_rect(screen_w * 0.01, screen_h * 0.75)
+        return bg_image, pixels
+    return bg_image, pixels
+
+
+def screen_init(pygame):
+    # получаем размер экрана
+    screen_info = pygame.display.Info()
+    screen_w = screen_info.current_w
+    screen_h = screen_info.current_h
+    # растягиваем окно во весь экран
+    screen = pygame.display.set_mode((screen_w, screen_h), pygame.FULLSCREEN)
+
+    # устанавливаем название окна
+    pygame.display.set_caption('Game')
+
+    all_sprites = pygame.sprite.Group()
+
+    # растянутый задний фон в ч/б (границы ходьбы) преобразуем в PixelArray
+    wb_bg_image = load_image("wb_backround.jpg")
+    pixels = pygame.PixelArray(pygame.transform.scale(wb_bg_image, (screen_w, screen_h)))
+    return screen, pixels, all_sprites, screen_w, screen_h
+
+
+def objects_init(pygame, screen_w, screen_h):
+    # получаем и растягиваем фон
+    bg_image = "backround.jpg"
+    bg = Object()
+    bg.image = pygame.transform.scale(load_image(bg_image), (screen_w, screen_h))
+    bg.rect = bg.image.get_rect()
+    bg.set_rect(0, 0)
+
+    # Здесь добавляются разные герои
+
+    # Первый объект Hero
+    hero = Hero()
+    hero_image = load_image("hero.jpg")
+    hero.image = pygame.transform.scale(hero_image, (dS, dS))
+    hero.rect = hero.image.get_rect()
+    # засовываем картинку героя в квадрат dSxdS (175х175)
+    # начальные координаты левого верхнего угла прямоугольной области для персонажа
+    hero.set_rect(screen_w * 0.75, screen_h * 0.75)
+
+    # Второй объект
+    apple = Entity()
+    apple.image = pygame.transform.scale(load_image("apple.jpg"), (100, 100))
+    apple.rect = apple.image.get_rect()
+    apple.set_rect(250, 250)
+
+    # Возврат героя и списка всех objects
+    return hero, bg, bg_image, [apple]
+
+
+def game_init(screen, hero, bg, all_sprites, objects, screen_w, screen_h):
+    all_sprites.add(bg)
+    for i in objects:
+        all_sprites.add(i)
+    all_sprites.add(hero)
+    all_sprites.draw(screen)
+
+    clock = pygame.time.Clock()
+           
+    # isStep = False - маркер приостаноки, т. е. требуется обход препятствия (текущая пиксела не валидная)
+    isStep = True
+    # новые требуемые координаты героя совпадают с собственными координатами героя
+    cords = (screen_w * 0.75, screen_h * 0.75)
+    running = True
+    return running, isStep, clock, cords
--- master.py
+++ master.py
@@ -1,6 +1,6 @@
 import pygame
 
-from core.handlers.base import corners, load_image
+from core.handlers.base import corners, screen_init, objects_init, game_init, event_handling
 from core.handlers.items import Hero, Object, Entity
 from core.data.constant import tk, dS
 
@@ -9,105 +9,46 @@ def main():
     pygame.init()
 
     # получаем размер экрана
-    screen_info = pygame.display.Info()
-    screen_w = screen_info.current_w
-    screen_h = screen_info.current_h
+    # Конфигурация экрана
+    screen, pixels, all_sprites, screen_w, screen_h = screen_init(pygame)
 
-    # растягиваем окно во весь экран
-    screen = pygame.display.set_mode((screen_w, screen_h), pygame.FULLSCREEN)
-    pygame.display.set_caption('Game')
+    # Получение всех героев в кортеже
+    hero, bg, bg_image, objects = objects_init(pygame, screen_w, screen_h)
 
-    all_sprites = pygame.sprite.Group()
+    # Задание значений игровых переменных
+    running, barrier, clock, cords = game_init(screen, hero, bg, all_sprites, objects, screen_w, screen_h)
 
-    # получаем и растягиваем картинку на весь экран
-    bg = Object()
-    bg_image = "backround.jpg"
-    bg.image = pygame.transform.scale(load_image(bg_image), (screen_w, screen_h))
-    bg.rect = bg.image.get_rect()
-    bg.set_rect(0, 0)
-
-    # растянутый задний фон в ч/б (границы ходьбы)
-    wb_bg_image = pygame.transform.scale(load_image("wb_backround.jpg"), (screen_w, screen_h))
-    pixels = pygame.PixelArray(wb_bg_image)
-
-    clock = pygame.time.Clock()
-
-    hero = Hero()
-    hero.image = pygame.transform.scale(load_image("hero.jpg"), (dS, dS))
-    hero.rect = hero.image.get_rect()
-    hero.set_rect(screen_w * 0.75, screen_h * 0.75)
-
-    items = Entity()
-    items.image = pygame.transform.scale(load_image("i.jpg"), (100, 100))
-    items.rect = items.image.get_rect()
-    items.set_rect(250, 250)
-
-    all_sprites.add(bg)
-    all_sprites.add(hero)
-    # all_sprites.add(items)
-
-    all_sprites.draw(screen)
-    # fd = False - маркер того, что требуется обход препятствия (текущая пиксела не валидная)
-    fd = True
-    # задаем "первый клик"
-    cords = (screen_w * 0.75, screen_h * 0.75)
-    running = True
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            # выход из программы по клавише Esc
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                # задаем корды, к который пойдет герой
-                cords = event.pos
-                if (pixels[cords] == 254 and hero.get_cords()[0] < screen_w * 0.2 and
-                        bg_image != "background_river.jpg"):
-                    bg_image = "background_river.jpg"
-                    bg.image = pygame.transform.scale(load_image(bg_image), (screen_w, screen_h))
-                    wb_bg_image = pygame.transform.scale(load_image("wb_background_river.jpg"), (screen_w, screen_h))
-                    pixels = pygame.PixelArray(wb_bg_image)
-                    hero.change_rect(screen_w * 0.85, screen_h * 0.75)
-                if (pixels[cords] == 254 and hero.get_cords()[0] > screen_w * 0.7 and
-                        bg_image != "backround.jpg"):
-                    bg_image = "backround.jpg"
-                    bg.image = pygame.transform.scale(load_image(bg_image), (screen_w, screen_h))
-                    wb_bg_image = pygame.transform.scale(load_image("wb_backround.jpg"), (screen_w, screen_h))
-                    pixels = pygame.PixelArray(wb_bg_image)
-                    hero.change_rect(screen_w * 0.01, screen_h * 0.75)
-                # при необходимости переворачиваем героя
-                hero.need_rotate(cords)
+        running, cords, bg_image, pixels = event_handling(pygame.event.get(), hero, bg, bg_image, pixels, cords,
+                                                          screen_w, screen_h)
 
         # смотрим, является ли пиксель по цвету в ч\б фоне черным (равен 0), иначе ничего не делаем
-        if pixels[cords] == 0:
+        if pixels[cords] == 0 and hero.need_step(cords):
             # меняем корды героя, если хоть одна отличается от кордов клика
-            if hero.need_step(cords):
-                x, y = 0, 0
 
-                # проверяем, обходит ли герой в данный момент препятствие
-                if fd:
-                    # делаем шаг
-                    x, y = hero.next_step(cords, pixels)
+            # проверяем, обходит ли герой в данный момент препятствие
+            if barrier:
+                # делаем шаг
+                isImpasse = hero.next_step(cords, pixels)
 
-                # ВАЖНО! если после тика корды не поменялись, а мы всё равно прошли через верхнее условие,
-                # то наш перс стоит в тупике, ниже код обхода этого тупика
-                if x == 0 and y == 0:
-                    # если fd = False, наш герой уже обходит препятствие
-                    if fd:
-                        # в corners проверяем различные ситуации, когда обходить надо по разному
-                        dx, dy = corners((hero.centralX(), hero.centralY()), cords)
+            # ВАЖНО! если после тика корды не поменялись, а мы всё равно прошли через верхнее условие,
+            # то наш перс стоит в тупике, ниже код обхода этого тупика
+            if isImpasse:
+                # если barrier = False, наш герой уже обходит препятствие
+                if barrier:
+                    # в corners проверяем различные ситуации, когда обходить надо по разному
+                    dx, dy = corners((hero.centralX(), hero.centralY()), cords)
 
-                    # меняем корды героя на dx, dy, если возвращается True, мы обошли прпятствие,
-                    # иначе повторяем код со следующим тиком
-                    fd = hero.overcome_step(pixels, dx, dy)
+                # меняем корды героя на dx, dy, если возвращается True, мы обошли прпятствие,
+                # иначе повторяем код со следующим тиком
+                barrier = hero.overcome_step(pixels, dx, dy)
+
+        hero.need_rotate(cords)
 
         all_sprites.draw(screen)
 
         clock.tick(tk)
+
         pygame.display.flip()
 
     pygame.quit()
